b97517c5d32f3004504ed1f6991d8ea9853247b1
ANDREW <146983052+andrewanuga@users.noreply.github.com>
2025-02-05 15:49:40 +0100

    Add files via upload

---
+++ Bio Project/script.js
@@ -0,0 +1,19 @@
+// const typewriter = document.querySelector('.typewriter');
+// const text = typewriter.textContent;
+// typewriter.textContent = '';
+
+// let i = 0;
+// const typingInterval = 35; // Adjust typing speed (milliseconds)
+
+// const type = (ele) => {
+//   if (i < text.length) {
+//       // Add normal characters
+//       ele.innerHTML += text.charAt(i);
+//       i++;
+
+//     // Continue typing
+//     setTimeout(type, typingInterval);
+//   }
+// };
+
+// type(typewriter);
